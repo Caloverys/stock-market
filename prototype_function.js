8b1328544fb7a806b5bef010f54a272b7fba8e56
Caloverys <79812606+Caloverys@users.noreply.github.com>
2022-06-25 20:17:24 -0400

    Update prototype_function.js

--- prototype_function.js
+++ prototype_function.js
@@ -51,57 +51,93 @@ String.prototype.return_color_in_rgba =  function(){
 
 
 
-function return_vertical_linear_garident(color) {
+function return_vertical_linear_garident(is_hover) {
     
   const canvasHeight = parseInt(window.getComputedStyle(parent_of_canvas).getPropertyValue('height'))
   const gradient = context.createLinearGradient(0, 0, 0, canvasHeight);
   const color = color.return_color_in_rgba();
-  if (color) {
-    gradient.addColorStop(0, hover_color);
-    gradient.addColorStop(0.3, "rgba(82,196,250,0.3)");
-    gradient.addColorStop(0.3, "rgba(82,196,250,0.3)");
-    gradient.addColorStop(1, 'transparent')
+  if (is_hover) {
+    gradient.addColorStop(0, `rgba(${hover_color},0.8)`);
+    gradient.addColorStop(0.3, `rgba(${hover_color},0.3)`);
+    gradient.addColorStop(0.3, `rgba(${hover_color},0.3)`);
 
-  } else if (return_color().includes("red")) {
-    gradient.addColorStop(0, "rgba(255,0,0,0.8)");
-    gradient.addColorStop(0.3, "rgba(255,0,0,0.3)");
-    gradient.addColorStop(0.3, "rgba(255,0,0,0.3)");
-    gradient.addColorStop(1, 'transparent')
+  } else{
 
-    gradient.addColorStop(1, 'transparent')
-  } else if (return_color().includes("green")) {
-    gradient.addColorStop(0, "rgba(124,252,0,0.8)");
-    gradient.addColorStop(0.3, "rgba(124,252,0,0.3)");
-    gradient.addColorStop(0.3, "rgba(124,252,0,0.3)");
-    gradient.addColorStop(1, 'transparent')
-  }
+  	if (return_color()) {
+  	    gradient.addColorStop(0, `rgba(${earn_money_color},0.8)`);
+        gradient.addColorStop(0.3, `rgba(${earn_money_color},0.3)`);
+        gradient.addColorStop(0.3, `rgba(${earn_money_color},0.3)`);
+
+    } else{
+  	    gradient.addColorStop(0, `rgba(${lose_money_color},0.8)`);
+        gradient.addColorStop(0.3, `rgba(${lose_money_color},0.3)`);
+        gradient.addColorStop(0.3, `rgba(${lose_money_color},0.3)`);
+    }
+  } 
+  gradient.addColorStop(1, 'transparent')
   return gradient
 
 }
 
 
-function return_horizontal_gradient(color, pos_start, pos_end) {
+function return_horizontal_gradient(is_earning, pos_start, pos_end) {
   const horizontal_Gradient = context.createLinearGradient(0, pos_start, 0, pos_end)
 
-  if (color === "red") {
-    horizontal_Gradient.addColorStop(0, "rgba(255,0,0,0.3)");
-    horizontal_Gradient.addColorStop(0.3, "rgba(255,0,0,0.1)");
-    horizontal_Gradient.addColorStop(0.3, "rgba(255,0,0,0.15)");
-    horizontal_Gradient.addColorStop(1, 'transparent')
+  if (is_earning) {
 
-  } else if (color === 'lawngreen') {
-    horizontal_Gradient.addColorStop(0, "rgba(124,252,0,0.3)");
-    horizontal_Gradient.addColorStop(0.3, "rgba(124,252,0,0.1)");
-    horizontal_Gradient.addColorStop(0.3, "rgba(124,252,0,0.15)");
-    horizontal_Gradient.addColorStop(1, 'transparent')
+  	 horizontal_Gradient.addColorStop(0, `rgba(${earn_money_color},0.3)`);
+        horizontal_Gradient.addColorStop(0.3, `rgba(${earn_money_color},0.1)`);
+       horizontal_Gradient.addColorStop(0.3, `rgba(${earn_money_color},0.15)`);
+
+  } else {
+
+   horizontal_Gradient.addColorStop(0, `rgba(${lose_money_color},0.3)`);
+        horizontal_Gradient.addColorStop(0.3, `rgba(${lose_money_color},0.1)`);
+        horizontal_Gradient.addColorStop(0.3, `rgba(${lose_money_color},0.15)`);
+
 
   }
+   horizontal_Gradient.addColorStop(1, 'transparent')
   return horizontal_Gradient
 }
 
+String.prototype.size_calculation = function(fontSize){
+  const div = document.body.appendChild(document.createElement('div'));
+  div.textContent = this;
+  div.style.cssText = `
+  font-size:${fontSize}px;
+  width:auto;
+  position:absolute;
+  visibility:hidden;
+  `;
+  //object destrusturing: const {} = object; ES6 feature 
+  const {width,height} = window.getComputedStyle(div);
+
+  div.remove();
+  return ({
+    width: parseFloat(width),
+    height: parseFloat(height)
+  })
+}
+
+
+Object.prototype.format_date = function(){
+  /*
+
+  Some broswers don't recognize a speicific date format 
+
+  Example: 2022-06-10 10:45:00 
+
+  This will return Invalid Date in safari 
+
+  The always best way is to reformat to ISO string 
 
 
 
+  */
+
+  return new Date(dateobject.substring(0, 4), dateobject.substring(5, 7) - 1, dateobject.substring(8, 10), dateobject.substring(11, 13), dateobject.substring(14, 16), "00")
+}
 
 
 
