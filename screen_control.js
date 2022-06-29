4cdf7b63fec70d97d3a867b20be43d7de6d27cb9
Caloverys <79812606+Caloverys@users.noreply.github.com>
2022-06-29 13:16:11 -0400

    Update screen_control.js

--- screen_control.js
+++ screen_control.js
@@ -2,30 +2,11 @@ const innerHeight = window.innerHeight;
 const innerWidth = window.innerWidth;
 let interval,is_substring;;
  if(innerWidth > 1200) is_substring = false
-      else is_substring = true
-
-if(!global_time) fecth_global_time().then(function(){
-	assign_interval()
-});
-else assign_interval();
+      else is_substring = true;
 
 
- 
- function fecth_global_time(){
- return fetch("https://worldtimeapi.org/api/timezone/america/new_york")
+(function(){
 
-    .then(res => res.json())
-
-    .then(time =>{
-    	console.log(time)
-
-    	global_time = new Date(time.datetime);
-    	        market_status_element.textContent = global_time.return_market_status() ? "Market Open" : "Market Closed";
-        time_element.textContent = (is_substring ?  (global_time.toString().split(" GMT")[0] + ' EDT').substring(16) :  global_time.toString().split(" GMT")[0] + ' EDT');
-})
- }
-
- function assign_interval(){
 if(interval) window.clearInterval(interval);
 
    interval = setInterval(() => {
@@ -34,4 +15,5 @@ if(interval) window.clearInterval(interval);
       market_status_element.textContent = global_time.return_market_status() ? "Market Open" : "Market Closed";
       time_element.textContent = (is_substring ?  (global_time.toString().split(" GMT")[0] + ' EDT').substring(16) :  global_time.toString().split(" GMT")[0] + ' EDT');
       }, 1000)
- }
+ 
+})()
